05d6d48e4fb2f63fda209b510d13da08ce60b270
Le_Mocha <60754513+LeMocha@users.noreply.github.com>
2021-06-30 10:17:42 +0400

    V1.3.0
    Le fichier ne s'est pas update avec le précédent pull.

--- bot.js
+++ bot.js
@@ -1,17 +1,20 @@
-const Discord = require('discord.js');
+const { Collection, Client } = require('discord.js');
+const { logTime } = require('./utils/utils');
+const { red } = require('chalk');
 const fs = require('fs');
 
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const raw = fs.readFileSync('config.json');
+const disbut = require('discord-buttons')(client);
 
-client.commands = new Discord.Collection();
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.reactions = new Discord.Collection();
-const reactionFiles = fs.readdirSync('./reactions').filter(file => file.endsWith('.js'));
+client.scommands = new Collection();
+const scommandFiles = fs.readdirSync('./slash-commands').filter(file => file.endsWith('.js'));
 
-client.rreactions = new Discord.Collection();
-const rreactionFiles = fs.readdirSync('./r-reactions').filter(file => file.endsWith('.js'));
+client.buttoncommand = new Collection();
+const buttonFiles = fs.readdirSync('./buttons').filter(file => file.endsWith('.js'));
 
 client.config = JSON.parse(raw);
 client.queue = new Map();
@@ -23,14 +26,16 @@ for (const file of commandFiles) {
     commandNumber++;
 }
 client.commandNumber = commandNumber
-for (const file of reactionFiles) {
-    const reaction = require(`./reactions/${file}`);
-    client.reactions.set(reaction.name, reaction);
+for (const file of scommandFiles) {
+    const scommand = require(`./slash-commands/${file}`);
+    client.scommands.set(scommand.name, scommand);
 }
-for (const file of rreactionFiles) {
-    const rreaction = require(`./r-reactions/${file}`);
-    client.rreactions.set(rreaction.name, rreaction);
+for (const file of buttonFiles) {
+    const bt = require(`./buttons/${file}`);
+    client.buttoncommand.set(bt.name, bt);
 }
+
+client.musicinteraction = null
 
 fs.readdir('./events/', (err, files) => {
     if (err) return console.error(err); 
@@ -43,11 +48,43 @@ fs.readdir('./events/', (err, files) => {
         const once = eventFunction.once;
 
         try {
-            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.execute(client, ...args, event));
+            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.execute(client, ...args));
         } catch (error) {
             console.error(error.stack);
         }
     });
+});
+
+client.ws.on('INTERACTION_CREATE', async (interaction) => {
+    if(interaction.type == 3) {
+
+        const buttonName = interaction.data.custom_id;
+    
+        const buttoncommand = client.buttoncommand.get(buttonName)
+        if (!buttoncommand) return;
+    
+        try {
+            buttoncommand.execute(interaction, client)
+        } catch (error) {
+            console.log(`[ERROR] [${logTime()}]`, red("Une erreur s'est produite lors de l'exécution d'une commande."))
+            console.error(error);
+        }
+        
+    } else {
+
+        const scommandName = interaction.data.name.toLowerCase();
+    
+        const scommand = client.scommands.get(scommandName)
+        if (!scommand) return;
+    
+        try {
+            scommand.execute(interaction, client)
+        } catch (error) {
+            console.log(`[ERROR] [${logTime()}]`, red("Une erreur s'est produite lors de l'exécution d'une commande."))
+            console.error(error);
+        }
+
+    }
 });
 
 client.login(client.config.token);
