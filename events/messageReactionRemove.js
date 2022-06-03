4b22e6706a9243d4c6cf0adce455a1957da1dcc6
Le_Mocha <60754513+LeMocha@users.noreply.github.com>
2022-06-03 19:05:06 +0400

    Refonte
    Push des fichiers initiaux

--- events/messageReactionRemove.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    async execute(client, reaction, user, event) {
-
-        return;
-        
-    }
-};
---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, Intents, Message } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const eventsPath = path.join(__dirname, 'src/events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(process.env.DISCORD_TOKEN);
